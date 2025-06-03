3100efceea48efcc6c166c6c6179e2f1f8cc8285
Phan Minh Khoi <pmkhoi2420@apcs.fitus.edu.vn>
2025-06-03 16:27:36 +0700

    Itterator implementation

---
+++ Iterator/main.cpp
@@ -0,0 +1,293 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+
+using namespace std;
+
+// ========================================= DECLARATIONS =========================================
+
+// DATA CLASSES DECLARATION
+class Song {
+public:
+    string title;
+    string artist;
+    int length; // in seconds
+    int popularity; // a score from 1 to 100
+
+    Song(const string& t, const string& a, int l, int p);
+};
+
+// INTERFACE CLASSES DECLARATIONS
+template <typename T>
+class Iterator {
+public:
+    virtual bool hasNext() const = 0;
+    virtual void first() = 0;
+    virtual void next() = 0;
+    virtual const T& current() const = 0;
+    virtual ~Iterator() = default;
+};
+
+template <typename T>
+class Container {
+public:
+    virtual Iterator<T>* createAlphabeticIterator() const = 0;
+    virtual Iterator<T>* createLengthIterator() const = 0;
+    virtual Iterator<T>* createPopularityIterator() const = 0;
+    virtual ~Container() = default;
+};
+
+// FORWARD DECLARATIONS
+template <typename T>
+class Playlist;
+
+// CONCRETE CLASSES DECLARATIONS
+template <typename T>
+class AlphabeticIterator : public Iterator<T> {
+public:
+    AlphabeticIterator(const Playlist<T>& playlist);
+    void first() override;
+    void next() override;
+    bool hasNext() const override;
+    const T& current() const override;
+
+private:
+    vector<int> index;
+    vector<int>::iterator myIt;
+    const Playlist<T>& playlistRef;
+};
+
+template <typename T>
+class LengthIterator : public Iterator<T> {
+public:
+    LengthIterator(const Playlist<T>& playlist);
+    void first() override;
+    void next() override;
+    bool hasNext() const override;
+    const T& current() const override;
+
+private:
+    vector<int> index;
+    vector<int>::iterator myIt;
+    const Playlist<T>& playlistRef;
+};
+
+template <typename T>
+class PopularityIterator : public Iterator<T> {
+public:
+    PopularityIterator(const Playlist<T>& playlist);
+    void first() override;
+    void next() override;
+    bool hasNext() const override;
+    const T& current() const override;
+
+private:
+    vector<int> index;
+    vector<int>::iterator myIt;
+    const Playlist<T>& playlistRef;
+};
+
+template <typename T>
+class Playlist : public Container<T> {
+public:        
+    Playlist() = default;
+    ~Playlist() override = default;
+    
+    void addToTheList(const T& item);
+    Iterator<T>* createAlphabeticIterator() const override;
+    Iterator<T>* createLengthIterator() const override;
+    Iterator<T>* createPopularityIterator() const override;
+
+    friend class AlphabeticIterator<T>;
+    friend class LengthIterator<T>;
+    friend class PopularityIterator<T>;
+
+private:
+    vector<T> privatePlaylist;
+};
+
+// FUNCTION DECLARATIONS
+void ClientCode();
+
+// ========================================= IMPLEMENTATIONS =========================================
+
+// SONG CLASS IMPLEMENTATION
+Song::Song(const string& t, const string& a, int l, int p)
+    : title(t), artist(a), length(l), popularity(p) {}
+
+// ALPHABETIC ITERATOR IMPLEMENTATION
+template <typename T>
+AlphabeticIterator<T>::AlphabeticIterator(const Playlist<T>& playlist) : playlistRef(playlist) {
+    int tempSize = playlist.privatePlaylist.size();
+    for (int i = 0; i < tempSize; i++){
+        index.push_back(i);
+    }
+    
+    // Sort indices based on alphabetical order of song titles
+    sort(index.begin(), index.end(), [&playlist](int a, int b) {
+        return playlist.privatePlaylist[a].title < playlist.privatePlaylist[b].title;
+    });
+
+    myIt = index.begin();
+}
+
+template <typename T>
+void AlphabeticIterator<T>::first() {
+    myIt = index.begin();
+}
+
+template <typename T>
+void AlphabeticIterator<T>::next() {
+    myIt++;
+}
+
+template <typename T>
+bool AlphabeticIterator<T>::hasNext() const {
+    return (myIt != index.end());
+}
+
+template <typename T>
+const T& AlphabeticIterator<T>::current() const {
+    if (myIt == index.end()) {
+        throw out_of_range("Iterator has reached the end.");
+    }
+    return playlistRef.privatePlaylist[*myIt];
+}
+
+// LENGTH ITERATOR IMPLEMENTATION
+template <typename T>
+LengthIterator<T>::LengthIterator(const Playlist<T>& playlist) : playlistRef(playlist) {
+    int tempSize = playlist.privatePlaylist.size();
+    for (int i = 0; i < tempSize; i++){
+        index.push_back(i);
+    }
+    
+    // Sort indices based on length order of songs
+    sort(index.begin(), index.end(), [&playlist](int a, int b) {
+        return playlist.privatePlaylist[a].length > playlist.privatePlaylist[b].length;
+    });
+
+    myIt = index.begin();
+}
+
+template <typename T>
+void LengthIterator<T>::first() {
+    myIt = index.begin();
+}
+
+template <typename T>
+void LengthIterator<T>::next() {
+    myIt++;
+}
+
+template <typename T>
+bool LengthIterator<T>::hasNext() const {
+    return (myIt != index.end());
+}
+
+template <typename T>
+const T& LengthIterator<T>::current() const {
+    if (myIt == index.end()) {
+        throw out_of_range("Iterator has reached the end.");
+    }
+    return playlistRef.privatePlaylist[*myIt];
+}
+
+// POPULARITY ITERATOR IMPLEMENTATION
+template <typename T>
+PopularityIterator<T>::PopularityIterator(const Playlist<T>& playlist) : playlistRef(playlist) {
+    int tempSize = playlist.privatePlaylist.size();
+    for (int i = 0; i < tempSize; i++){
+        index.push_back(i);
+    }
+    
+    // Sort indices based on popularity order of songs
+    sort(index.begin(), index.end(), [&playlist](int a, int b) {
+        return playlist.privatePlaylist[a].popularity > playlist.privatePlaylist[b].popularity;
+    });
+
+    myIt = index.begin();
+}
+
+template <typename T>
+void PopularityIterator<T>::first() {
+    myIt = index.begin();
+}
+
+template <typename T>
+void PopularityIterator<T>::next() {
+    myIt++;
+}
+
+template <typename T>
+bool PopularityIterator<T>::hasNext() const {
+    return (myIt != index.end());
+}
+
+template <typename T>
+const T& PopularityIterator<T>::current() const {
+    if (myIt == index.end()) {
+        throw out_of_range("Iterator has reached the end.");
+    }
+    return playlistRef.privatePlaylist[*myIt];
+}
+
+// PLAYLIST CLASS IMPLEMENTATION
+template <typename T>
+void Playlist<T>::addToTheList(const T& item) {
+    privatePlaylist.push_back(item);
+}
+
+template <typename T>
+Iterator<T>* Playlist<T>::createAlphabeticIterator() const {
+    return new AlphabeticIterator<T>(*this);
+}
+
+template <typename T>
+Iterator<T>* Playlist<T>::createLengthIterator() const {
+    return new LengthIterator<T>(*this);
+}
+
+template <typename T>
+Iterator<T>* Playlist<T>::createPopularityIterator() const {
+    return new PopularityIterator<T>(*this);
+}
+
+// CLIENT CODE IMPLEMENTATION
+void ClientCode() {
+    Playlist<Song> myFavoriteSongs;
+    myFavoriteSongs.addToTheList(Song("Bohemian", "a", 354, 1000));
+    myFavoriteSongs.addToTheList(Song("Stairway", "b", 482, 800));
+    myFavoriteSongs.addToTheList(Song("Imagined", "c", 183, 1200));
+    myFavoriteSongs.addToTheList(Song("Hey Jude", "d", 431, 900));
+
+    Iterator<Song>* AIt = myFavoriteSongs.createAlphabeticIterator();
+    Iterator<Song>* LIt = myFavoriteSongs.createLengthIterator();
+    Iterator<Song>* PIt = myFavoriteSongs.createPopularityIterator();
+
+    for (AIt->first(); AIt->hasNext(); AIt->next()) {
+        cout << "Alphabetic: " << AIt->current().title << "\t\t" << AIt->current().artist << "\t\t" << AIt->current().length << "\t\t" << AIt->current().popularity << endl;
+    }
+    cout << endl;
+
+    for (LIt->first(); LIt->hasNext(); LIt->next()) {
+        cout << "Length: " << LIt->current().title << "\t\t" << LIt->current().artist << "\t\t" << LIt->current().length << "\t\t" << LIt->current().popularity << endl;
+    }
+
+    cout << endl;    
+    
+    for (PIt->first(); PIt->hasNext(); PIt->next()) {
+        cout << "Popularity: " << PIt->current().title << "\t\t" << PIt->current().artist << "\t\t" << PIt->current().length << "\t\t" << PIt->current().popularity << endl;
+    }
+
+    // Clean up memory
+    delete AIt;
+    delete LIt;
+    delete PIt;
+}
+
+int main() {
+    ClientCode();
+    return 0;
+}
